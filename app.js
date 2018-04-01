d4821df45e6a987e26d83db4015b5ed2fa95e189
Ruben Alliet <allietruben@gmail.com>
2018-04-02 00:46:35 +0200

    handlebars css link fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 // static folder
-app.set('/public',express.static(path.join(__dirname,'public')));
+app.use(express.static(__dirname + '/public'));
 
 //Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
-    res.send("hello");
+    res.render('contact');
 })
 
 app.listen(3000,() => {
