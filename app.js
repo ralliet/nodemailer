6b39f5d8e3d8d6092078f905b4dafe32e6342db1
Ruben Alliet <allietruben@gmail.com>
2018-04-02 15:13:18 +0200

    working nodemailer

--- app.js
+++ app.js
@@ -14,19 +14,69 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
 //Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.render('contact');
 })
 
-app.post('/send',(req,res) => {
-    console.log(req.body);
+app.post('/send', (req, res) => {
+    const html = `
+        <p><strong>You received a new message:</strong></p>
+        <ul>
+            <li>Name: ${req.body.name}</li>
+            <li>Company: ${req.body.company}</li>
+            <li>Email: ${req.body.email}</li>
+            <li>phone: ${req.body.phone}</li>
+            <li>Message: ${req.body.message}</li>
+        </ul>`;
+
+    nodemailer.createTestAccount((err, account) => {
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: account.user, // generated ethereal user
+                pass: account.pass // generated ethereal password
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
+
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Fred Foo 👻" <foo@example.com>', // sender address
+            to: 'rubenalliet@hotmail.com', // list of receivers
+            subject: 'Hello ✔', // Subject line
+            text: 'Hello world?', // plain text body
+            html: html // html body
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            // Preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+            res.render('contact',{msg: "email has been sent"});
+
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        });
+    });
 })
 
-app.listen(3000,() => {
+app.listen(3000, () => {
     console.log('server is listening on port 3000')
 })
 
